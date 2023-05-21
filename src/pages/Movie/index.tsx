ad86e63dfb2aa010e2cc981066567362ff5a1910
Rudinei de Brito <rudineidebrito18@gmail.com>
2023-05-21 09:24:24 -0300

    update movie page

--- src/pages/Movie/index.tsx
+++ src/pages/Movie/index.tsx
@@ -6,6 +6,9 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import api from '../../services/api'
 
+import './style.css'
+
+import { FaCalendarAlt, FaStar } from 'react-icons/fa'
 import noImage from '../../assets/images/no-image.png'
 const imgUrl = import.meta.env.VITE_IMG
 
@@ -28,17 +31,34 @@ function Movie() {
 		<>
 			<Navbar />
 			<Area>
-				<img 
-					src={movie?.poster_path ? imgUrl + movie.poster_path : noImage} 
-					alt={`${movie?.poster_path} poster`} 
-				/>
-				<h1>{movie?.title}</h1>
-				<p>{movie?.overview}</p>
-				<p>{movie?.runtime}</p>
-				<p>{movie?.release_date}</p>
-				<p>{movie?.vote_average}</p>
-				<p>{movie?.vote_count}</p>
-				{movie?.genres.length ? movie.genres.map((e) => <p key={e.id}>{e.name}</p>) : ''}
+				<div className='movieContainer'>
+					<img
+						src={movie?.poster_path ? imgUrl + movie.poster_path : noImage}
+						alt={`${movie?.poster_path} poster`}
+						className='poster'
+					/>
+					<div className='infosContainer'>
+						<h1 className='title'>{movie?.title}</h1>
+						<div className='subInfos'>
+							<p className='overview'>
+								<h2>Overview:</h2>
+								{movie?.overview}
+							</p>
+							<div className='genres'>
+								<h2>Genres: </h2>
+								{movie?.genres.length ? movie.genres.map((e) => <p key={e.id}>{e.name}</p>) : ''}
+							</div>
+							<p className='runtime'>
+								<span>
+									Runtime: 
+								</span>
+								{` ${movie?.runtime}min`}
+							</p>
+							<p><FaStar className="stars" /> {`${movie?.vote_average.toFixed(1)}/10`}</p>
+							<p><FaCalendarAlt /> {movie?.release_date}</p>
+						</div>
+					</div>
+				</div>
 			</Area>
 		</>
 	)
